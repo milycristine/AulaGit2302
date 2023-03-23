6b65e69c9bbf48d4ec74006af63cfa8549bb6bd1
Auto Logon <prof-luiz@outlook.com>
2023-03-23 09:31:20 -0300

    criacão exemplo 17 e 18

---
+++ Aula06/exemplo17.js
@@ -0,0 +1,18 @@
+class livro{
+    constructor(pNome, pPreco){
+        this.nome= pNome
+        this.preco= pPreco
+    }
+    get Nome(){return this.nome }
+    set Nome(pNome){this.nome=pNome}
+
+    get Preco(){return this.preco }
+    set Preco(pPreco){this.preco=pPreco}
+
+    calcularDesconto(pDesconto){
+        this.preco=this.preco-pDesconto
+    }
+}
+var obj_livro1= new livro("Use a cabeça Java", 200)
+obj_livro1.calcularDesconto(30)
+console.log("O livro "+ obj_livro1.nome + " custa "+obj_livro1.preco+" reais")
